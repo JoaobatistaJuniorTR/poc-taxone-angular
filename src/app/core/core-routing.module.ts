607f8d65ca6633bb8997a7a100ac556cb2e2226a
Joao Batista Junior <joaobatista.junior@thomsonreuters.com>
2022-02-09 16:34:04 -0300

    feat: add page-not-found component

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -8,6 +9,10 @@ const routes: Routes = [
     path: 't1dw',
     loadChildren: () =>
       import('../features/features.module').then((m) => m.FeaturesModule),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,9 +6,10 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { ManagerComponent } from './components/manager/manager.component';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [NavBarComponent, ManagerComponent, HomeComponent],
+  declarations: [NavBarComponent, ManagerComponent, HomeComponent, PageNotFoundComponent],
   imports: [CommonModule, CoreRoutingModule, FormsModule, SharedModule],
   exports: [NavBarComponent, ManagerComponent],
 })
